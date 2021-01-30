da13270802940cf4aa6002df49ebeffad6362f8c
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-01-30 14:14:56 +0100

    Added styles, additional experimenting with JS, added server status indication

--- WebServer.py
+++ WebServer.py
@@ -12,19 +12,21 @@ SUBMIT_URL = "/submit_job"
 
 @app.route("/")
 def home_page():
-    return render_template("index.xhtml", submit_url=SUBMIT_URL)
+    # TODO: create function to fetch server info
+    return render_template("index.xhtml", submit_url=SUBMIT_URL, serv_info=get_server_info())
 
 @app.route("/new_job", methods=["GET"])
 def new_job():
-    return render_template("new_job.xhtml")
+    return render_template("new_job.xhtml", serv_info=get_server_info())
 
 @app.route(SUBMIT_URL, methods=["POST"])
 def submit_job():
     if request.method == "POST":
         return render_template("job_submitted.xhtml", job_id="TODO",
-                               submitted_data=request.form)
+                               submitted_data=request.form,
+                                serv_info=get_server_info())
     else:
-        return redirect(url_for("home_page"))
+        return redirect(url_for("home_page"), serv_info=get_server_info())
 
 @app.route("/status/<job_id>")
 def show_status(job_id):
@@ -36,18 +38,29 @@ def show_status(job_id):
 
     if found:
         return render_template("status_page.xhtml", job_id=job_id,
-                               status=status)
+                               status=status, serv_info=get_server_info())
         # TODO: create status template
 
-    return render_template("not_found.xhtml", job_id=job_id)
+    return render_template("not_found.xhtml", job_id=job_id,
+                           serv_info=get_server_info())
     # TODO: create not_found template
+
+def get_server_info() -> dict:
+    data = {"server_status": "running",
+            "queued": 2,
+            "running": 8,
+            "completed": 3418}
+
+    # TODO: actually create this one. Now dummy data
+
+    return data
 
 # Error handlers
 @app.errorhandler(404)
 def invalid_method(error):
     #logging.error(utils.fetch_base_error_message(error, request))
     # return redirect(url_for("home_page"))
-    return render_template("page_not_found.xhtml"), 404
+    return render_template("page_not_found.xhtml", serv_info=get_server_info()), 404
 
 @app.errorhandler(405)
 def page_not_found(error):
--- static/js/tester.js
+++ static/js/tester.js
@@ -16,4 +16,12 @@ function option5change(){
     enableOrDisableOption("option2", enable);
 }
 
+function validateForm(){
+    var x = document.forms["optionForm"]["e-value"];
+    console.log(x.value);
+    // TODO
+
+    return false;
+}
+
 // When option 5 becomes checked: uncheck option 1 and 2, and make them unclickable
