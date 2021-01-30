c32d77e4f5bf4716852b4961eecbece3073f40be
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-01-30 12:47:39 +0100

    Added JS to disable HTML elements onClick

--- static/js/tester.js
+++ static/js/tester.js
@@ -1,1 +1,19 @@
 console.log("We logged something")
+
+function enableOrDisableOption(id, enable) {
+    // For checkboxes
+    var elem = document.getElementById(id);
+
+    elem.checked = enable;
+    elem.disabled = !enable;
+}
+
+function option5change(){
+    var elem = document.getElementById("option5")
+    var enable = !elem.checked
+
+    enableOrDisableOption("option1", enable);
+    enableOrDisableOption("option2", enable);
+}
+
+// When option 5 becomes checked: uncheck option 1 and 2, and make them unclickable
