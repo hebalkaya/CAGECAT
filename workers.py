b46d8940d058071945bd9319441b7839e161cda5
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-03-01 14:50:34 +0100

    back-end search module: session file, clustering, binary table

--- workers.py
+++ workers.py
@@ -37,10 +37,10 @@ def dummy_sleeping(msg):
 
 
 def cblaster_search(job_id, options=None, file_path=None, prev_page=None):
-    sleep(10)
     base_path = f"{LOGGING_BASE_DIR}{sep}{job_id}"
+    results_path = f"{base_path}{sep}results{sep}"
+    logs_path = f"{base_path}{sep}logs{sep}"
 
-    all_keys = options.keys()
     #base_path = create_directories(job_id) # should probably be done when
     # getting the request to store the uploaded files
 
@@ -50,8 +50,10 @@ def cblaster_search(job_id, options=None, file_path=None, prev_page=None):
     # TODO: change -qf to uploaded file
     # create the basic command, with all required fields
     cmd = ["cblaster", "search",
-           "-qf", file_path,
-           "-o", f"{base_path}{sep}results{sep}{job_id}_summary.txt",
+           "--query_file", file_path,
+           "--output", f"{results_path}{job_id}_summary.txt",
+           "--session_file", f"{results_path}{job_id}_session.json"
+           # TODO: or add creating plot to standard options
            # "--database", options["database_type"],
            # "--entrez_query", options["entrez_query"],
            # "--hitlist_size", options["max_hits"]
@@ -63,96 +65,54 @@ def cblaster_search(job_id, options=None, file_path=None, prev_page=None):
     cmd.extend(["--max_evalue", options["max_evalue"],
                 "--min_identity", options["min_identity"],
                 "--min_coverage", options["min_query_coverage"]])
+
     # TODO: add clustering options
-    # TODO: add summary table
+    cmd.extend(["--gap", options["max_intergenic_gap"],
+                "--unique", options["min_unique_query_hits"],
+                "--min_hits", options["min_hits_in_clusters"]])
+
+    # add summary table
     sum_table_delim = options["searchSumTableDelim"]
     if sum_table_delim: # evalutes to True if not an empty string
         cmd.extend(["--output_delimiter", sum_table_delim])
 
     cmd.extend(["--output_decimals", options["searchSumTableDecimals"]])
 
-    if "searchSumTableHideHeaders" in all_keys:
+    if "searchSumTableHideHeaders" in options:
         cmd.append("--output_hide_headers")
-    # TODO: add binary table
-    # TODO: add additional options
+
+    # add binary table
+    cmd.extend(["--binary", f"{results_path}{job_id}_binary.txt"]) # create a binary table
+
+    bin_table_delim = options["searchBinTableDelim"]
+    if bin_table_delim:
+        cmd.extend(["--binary_delimiter", bin_table_delim])
+
+    cmd.extend(["--binary_decimals", options["searchBinTableDecimals"]])
+
+    if "searchBinTableHideHeaders" in options:
+        cmd.append("--binary_hide_headers")
+
+    cmd.extend(["--binary_key", options["keyFunction"]])
+
+    # TODO: maybe it can also be used with len as keyfunction
+    if "hitAttribute" in options: # only when keyFunction is not len
+        cmd.extend(["--binary_attr", options["hitAttribute"]])
+
+    # add additional options
+    if "sortClusters" in options:
+        cmd.append("--sort_clusters")
+
+    if "generatePlot" in options:
+        cmd.extend(["--plot", f"{results_path}{job_id}_plot.html"])
+
     program = cmd[0]
 
-    print(f"------------------\nCommand: {cmd}")
-    with open(f"{base_path}/logs/{job_id}_{program}.log", "w") as outf:
+    with open(f"{logs_path}{job_id}_cmd.txt", "w") as outf:
+        outf.write(f"Sent options:\n{options}\n\nCommand:\n{' '.join(cmd)}\n")
+
+    # print(f"------------------\nCommand: {cmd}")
+    with open(f"{logs_path}{job_id}_{program}.log", "w") as outf:
         subprocess.run(cmd, stderr=outf, stdout=outf, text=True)
 
     print("We are finished :)")
-
-
-def execute_cblaster(job_id, form=None, files=None, prev_page=None): #
-    # deprecated
-    sleep(randint(5, 12))
-    # Using None for every keyword parameter allows future calls to this
-    # function to exclude setting this keyword parameter when it is not
-    # required
-    base_path = create_directories(job_id)# TODO: uncomment for on linux
-
-    # if files is not None or not files:
-    #     for name, file in files.items():
-    #         safe_filename = file.filename #TODO: make it a safe filename
-    #         path = f"{base_path}/uploads/{safe_filename}"
-    #         file.save(path)
-    #         print(f"File saved: {path}")
-
-    # Prints are not shown unless the cmd is not executed, or after the cmd
-    # has finished
-    print(form)
-    print(files)
-    print(job_id)
-    # We can't do anything with the files because they are still part of the
-    # request. They are not yet saved on the server. Possible solution: save
-    # them from during request processing, and store links to the paths of
-    # the files in the file keyword to use it here
-    print(prev_page)
-
-    print("Going to execute cblaster now")
-    cmd = ["cblaster", "search", "-qf", "A0A411L027.1.fasta", "-o",
-           f"{base_path}/results/{job_id}_cblaster.json"] # -o should point
-    # to result directory
-    program = cmd[0] # for pipeline stage
-
-    with open(f"{base_path}/logs/{job_id}_{program}.log", "w") as outf: # should
-        # point
-        # to
-        # log directory
-        subprocess.run(cmd, stderr=outf, stdout=outf, text=True)
-
-
-# def execute_cblaster_cmd(req, job_id):
-#     # create_directories() TODO: create directories for this job
-#     print(req)
-#     print(job_id)
-#     # with open(f"./{job_id}_log.txt", "w") as logfile:
-#     #     print("We are here", file=logfile)
-#     previous_url = "/" + req.referrer.split("/")[-1]
-#     print(previous_url)
-#     sleep(5)
-#
-#         # if previous_url == url_for("create_database"):
-#         #     print("ok")
-#             # save_file("custom_databases", request.files.getlist(), app)
-#             # save_file(request.files.getlist(FILE_POST_FUNCTION_ID_TRANS[
-#             #                                     "create_database"]), app)
-#             # TODO: here we can do something
-#
-#
-#         # elif previous_url == url_for("calculate_neighbourhood"):
-#         # save_file(request.files.getlist(FILE_POST_FUNCTION_ID_TRANS[
-#         #                                 "calculate_neighbourhood"]), app)
-#
-#         # cmd = ["pip", "freeze"]
-#         #
-#         # subprocess.run(cmd, text=True, encoding="UTF-8")
-#         # cmd = ['cblaster', 'search', '-qf', '1yjy.fa', '-s', 'test_run2.json']
-#         #
-#         # with open(r"C:\Users\matth\OneDrive\Opleidingen\Master_WUR\__Thesis"
-#         #           r"\thesis_repo\outfile1.txt", "w") as outf:
-#         #
-#         #
-#         #     subprocess.run(cmd, stdout=outf, stderr=outf,
-        #                    text=True, encoding="UTF-8")
