7c6db981aa7100208357b0363aa7a39f92ae7d19
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-03-19 13:26:51 +0100

    fixed logging/result path problems, added extra documentation

--- multicblaster/workers.py
+++ multicblaster/workers.py
@@ -146,39 +146,51 @@ def cblaster_search(job_id, options=None, file_path=None, prev_page=None):
     if "sortClusters" in options:
         cmd.append("--sort_clusters")
 
-    program = cmd[0]
-
-    # with open(f"{logs_path}huh.txt", "w") as outf:
-    #     print(cmd, file=outf)
-    #     print("\n", file=outf)
-    #     print(options, file=outf)
-    #     print("\n", file=outf)
-    #     print(" ".join(cmd), file=outf)
-    log_settings(cmd, f"{LOG_PATH}{job_id}")
-    return_code = run_command(cmd, f"{LOG_PATH}{job_id}_{program}.log")
-
+    return_code = run_command(cmd, LOG_PATH, job_id)
     post_job_formalities(job_id, return_code)
 
 
-def generate_paths(job_id):
+def generate_paths(job_id: str) -> t.Tuple[str, str]:
+    """Returns paths for logging and result directories
+
+    Input:
+        - job_id: ID corresponding to the job the function is called for
+
+    Output:
+        - [0]: path for the logging directory
+        - [1]: path for the results directory
+    """
     base = os.path.join(LOGGING_BASE_DIR, job_id)
-    results = os.path.join(base, "results")
-    logs = os.path.join(base, "logs")
-    return logs, results
+    return os.path.join(base, "results"), os.path.join(base, "logs")
 
 
-def run_command(cmd, log_path):
+def run_command(cmd: t.List[str, ...], log_base: str, job_id: str) -> int:
+    """Executes a command on the command line
+
+    Input:
+        - cmd: splitted command to be executed where multiple elements in the
+            list are joined together with a space to form a full command
+        - log_base: base directory for logging. Has the following structure:
+            "multicblaster/jobs/{job_id}/logs/"
+        - job_id: ID corresponding to the job the function is called for
+
+    Output:
+        - res.returncode: exit code of the executed command. A non-zero exit
+            code indicates something went wrong. An exit code of 0 indicates
+            the command has executed without any problems.
+
     # TODO: add graceful termination handling by SIGTERM. When terminated
     # status should be changed to "failed" and "finish" time should be added
+    """
+    log_settings(cmd, log_base, job_id)
 
-    with open(log_path, "w") as outf:
+    with open(os.path.join(log_base, f"{job_id}_{cmd[0]}.log"), "w") as outf:
         res = subprocess.run(cmd, stderr=outf, stdout=outf, text=True)
 
     return res.returncode
 
-def log_settings(cmd,base_path):
-    with open(f"{base_path}_cmd.txt", "w") as outf:
-        # outf.write(f"Sent options:\n{options}\n\nCommand:\n{' '.join(cmd)}\n")
+def log_settings(cmd, log_base, job_id):
+    with open(os.path.join(log_base, f"{job_id}_cmd.txt"), "w") as outf:
         outf.write(" ".join(cmd))
 
 def cblaster_gne(job_id, options=None, file_path=None, prev_page=None):
@@ -206,10 +218,8 @@ def cblaster_gne(job_id, options=None, file_path=None, prev_page=None):
 
     # TODO: test if gne also works with a job_id
     cmd.extend(create_summary_table_commands('gne', options))
-    program = cmd[0]
 
-    log_settings(cmd, f"{LOG_PATH}{job_id}")
-    return_code = run_command(cmd, f"{LOG_PATH}{job_id}_{program}.log")
+    return_code = run_command(cmd, LOG_PATH, job_id)
 
     post_job_formalities(job_id, return_code)
 
@@ -220,7 +230,6 @@ def zip_results(job_id):
     # to_include = [os.path.join(base, "results"), os.path.join(base), "uploads"]
 
     cmd = ["zip", "-r", os.path.join("results", f"{job_id}.zip"), "."] # all files under the base folder
-
     run_command(cmd, os.path.join("logs", f"{job_id}_zip.txt"))
 
 
@@ -229,7 +238,9 @@ def pre_job_formalities(job_id):
     mutate_status(job_id, "start", db)
 
 def post_job_formalities(job_id, return_code):
+    zip_results(job_id)
+
     add_time_to_db(job_id, "finish", db)
     mutate_status(job_id, "finish", db, return_code=return_code)
 
-    zip_results(job_id)
+
