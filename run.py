6b5363e69882d819d2ded450e80c569f94a0eeb1
Matthias van den BElt <matthias.vandenbelt@wur.nl>
2021-05-24 22:00:01 +0200

    changed port

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from multicblaster import app
 
 ### main code
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5001)
 
     # lets other computers within the same network access the web pages
     # by typing the following address in a web browser:
