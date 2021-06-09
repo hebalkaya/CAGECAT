c9fbc401cb0531c6698f214c7fc5f9a5c266285e
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-06-09 17:21:57 +0200

    adding entering job details section in every downstream module

--- multicblaster/const.py
+++ multicblaster/const.py
@@ -29,7 +29,7 @@ DOWNSTREAM_MODULES_OPTIONS = {"search": ["recompute", "gne",
                               "clinker_query": ["extract_clusters"] # TODO: possibly also corason?
                               }
 
-JOB_DESCRIPTOR_HELPS = {'job_title': {'title': 'Job title', 'module': '', 'text': 'Enter a job title for easy identification of jobs.\n\nRequired: no'},
+JOB_DESCRIPTOR_HELPS = {'job_title': {'title': 'Job title', 'module': '', 'text': 'Enter a job title for easy identification of jobs.\n\nRequired: no\n\nMaximum length: 60 characters'},
                         'email_notification': {'title': 'Email notification', 'module': '', 'text': 'Enter your e-mail to get notified when your job has finished.\n\nRequired: no'}}
 GENERAL_HELPS = {'generalEnteredJobId': {'title': 'Previous job ID', 'module': '', 'text': 'The ID of the job which\' results are wished to be used.'},
                  'generalDelimiter': {'title': 'Output file delimiter', 'module': '', 'text': 'Single delimiter character to use when writing results to a file.\n\nResults will be separated of each other in the output file by the specified character.\n\nRequired: no\nDefault: no delimiter (human readable)'},
