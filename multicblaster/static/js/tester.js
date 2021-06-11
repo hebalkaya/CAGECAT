e2122f7f7345253075f2713a7802871c92f08f55
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-06-11 16:25:15 +0200

    help texts can now be shown by clicking on them (accordion style)

--- multicblaster/static/js/tester.js
+++ multicblaster/static/js/tester.js
@@ -1006,3 +1006,20 @@ function redirect(url){
         window.location.href = url;
     }, 50);
 }
+
+function addAccordionListeners() {
+    // from: https://www.w3schools.com/howto/howto_js_accordion.asp
+    let acc = document.getElementsByClassName("accordion");
+
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            let panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+}
