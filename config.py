2df82173fdfac8d2823658cdf6abafd6a3ad0c8e
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-06-28 16:50:16 +0200

    code documentation

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
-# config settings can be put here
+"""Module with changeable parameters if something would change in the future
 
+Author: Matthias van den Belt
+"""
 
 # changeable
 CONF = {"SQLALCHEMY_DATABASE_URI": 'sqlite:////repo/multicblaster/database.db',
         "SQLALCHEMY_TRACK_MODIFICATIONS": False,
-        "DEBUG": True,
-        "HOST": "0.0.0.0",
-        "PORT": 5001,
+        # "DEBUG": True,
+        # "HOST": "0.0.0.0",
+        # "PORT": 5001,
         "DOWNLOAD_FOLDER": "jobs",
         "DATABASE_FOLDER": "/lustre/BIF/nobackup/belt017/databases",
         'PFAM_DB_FOLDER': '/pfam_db',
@@ -28,15 +30,3 @@ BASE_URL = 'ftp.ncbi.nlm.nih.gov'
 BASE_DIR = '/lustre/BIF/nobackup/belt017/refseq_gbks'
 COMPLETE_DOWNLOADS_FILE = '/lustre/BIF/nobackup/belt017/complete_downloads.txt'
 SUCCESSFULL_DOWNLOADS_FN = 'successfull_downloads.txt'
-
-# DEBUG=True
-#
-# HOST="0.0.0.0"
-# PORT=5001
-
-# SQLALCHEMY_TRACK_MODIFICATIONS = False
-# DOWNLOAD_FOLDER = "jobs" # multicblaster not required in front of jobs
-#
-# # PFAM_FOLDER = '/lustre/BIF/nobackup/belt017/multicblaster_HMM'
-# # DATABASE_FOLDER = '/lustre/BIF/nobackup/belt017/multicblaster_HMM/databases'
-# DATABASE_FOLDER = '/storage/databases'
--- run.py
+++ run.py
@@ -12,11 +12,4 @@ if __name__ == "__main__":
     if not os.path.exists(app.config["DATABASE_FOLDER"]):
             os.makedirs(app.config["DATABASE_FOLDER"], exist_ok=True)
 
-    # TODO: should be from config file
-
-    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.run(host=app.config["HOST"], port=app.config["PORT"])
-
-    # lets other computers within the same network access the web pages
-    # by typing the following address in a web browser:
-    #       "<ip_of_pc_where_this_module_is_ran_from>:5001"
+    app.run()
