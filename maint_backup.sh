a7e431511dc14b7cb2a465fef8b6fc5b9a3ce8b3
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-07-26 11:27:49 +0200

    created backup script, including compression. changes to Dockerfile (create dirs in container when creating instead of copyingempty folders)

--- maint_backup.sh
+++ maint_backup.sh
@@ -1,3 +1,22 @@
 
-fp="$1 $(date +"%Y%m%d")"
-echo $fp
+fp="/backups/$(date +"%Y%m%d")_backup"
+
+echo "Creating directory: $fp"
+mkdir $fp
+
+echo "Copying jobs folder to $fp/jobs"
+cp -r /repo/cagecat/jobs "$fp"
+
+echo "Copying SQL database  to $fp/database.db"
+cp /repo/cagecat/database.db "$fp"
+
+echo "Copying Help template to $fp/help.xhtml"
+cp /repo/cagecat/templates/help.xhtml "$fp"
+
+echo "Copying logs folder to $fp/process_logs"
+cp -r /process_logs "$fp"
+
+echo "Compressing in to $fp.tar.gz"
+tar cvf - $fp --remove-files | gzip -9 - > $fp.tar.gz
+
+echo "Finished. Backup available at $1.tar.gz"
