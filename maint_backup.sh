032db650fe225c6b705aa14257a4acca57a53255
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-07-29 16:34:01 +0200

    update backup script to log installed apt packages and python libs (to get their versions)

--- maint_backup.sh
+++ maint_backup.sh
@@ -16,6 +16,14 @@ cp /repo/cagecat/templates/help.xhtml "$fp"
 echo "Copying logs folder to $fp/process_logs"
 cp -r /process_logs "$fp"
 
+pckgs_linux="$fp/linux_packages.txt"
+echo "Writing installed Linux packages to $pckgs_linux"
+apt list --installed > "$pckgs_linux"
+
+libs_python="$fp/python_libs.txt"
+echo "Writing installed Python libraries to $libs_python"
+pip freeze > "$libs_python"
+
 echo "Compressing in to $fp.tar.gz"
 tar cvf - $fp --remove-files | gzip -9 - > $fp.tar.gz
 
