3ce5b6306d495115c32b1bba37e8b09a7843bdf0
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-07-29 17:00:00 +0200

    update back up script to include where new version of CAGECATs version is stored

--- maint_backup.sh
+++ maint_backup.sh
@@ -10,8 +10,8 @@ cp -r /repo/cagecat/jobs "$fp"
 echo "Copying SQL database  to $fp/database.db"
 cp /repo/cagecat/database.db "$fp"
 
-echo "Copying Help template to $fp/help.xhtml"
-cp /repo/cagecat/templates/help.xhtml "$fp"
+echo "Copying config.py to $fp/config.py (CAGECAT's version number)"
+cp /repo/config_files/config.py "$fp"
 
 echo "Copying logs folder to $fp/process_logs"
 cp -r /process_logs "$fp"
