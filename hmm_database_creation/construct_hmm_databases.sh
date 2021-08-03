d5ee0dc8c666ce6ad884ff279989d14e3191e100
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-08-03 16:01:33 +0200

    add removing too_few_species.txt in constructing hmm script

--- hmm_database_creation/construct_hmm_databases.sh
+++ hmm_database_creation/construct_hmm_databases.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 # TODO: remove previous databases, create them all over again to include new genomes
+# TODO: find out how we can obtain the families of organisms from NCBI
 # TODO: we could also adapt this script to download fungi genomes
 
 fn_non_unique="non_unique_genera.txt"
@@ -8,6 +9,9 @@ fn_unique="unique_genera.txt"
 
 echo "If you try to rerun this script, make sure to remove any unmoved files.."
 echo ".. from the default RefSeq GBKS storage folder to prevent missing a file"
+
+echo "Removing too_few_species.txt"
+rm too_few_species.txt
 
 echo " Fetching all genera from NCBI"
 esearch -db assembly -query '(((prokaryota[orgn] AND ("representative genome"[refseq category] OR "reference genome"[refseq category])) AND (latest[filter] AND all[filter] NOT anomalous[filter])))' | efetch -format docsum | xtract -pattern DocumentSummary -element Organism > "$fn_non_unique"
@@ -35,6 +39,4 @@ python download_files.py 'everything_has_been_downloaded'
 echo "Removing ftp paths files"
 rm *_ftp_paths.txt
 
-# TODO: maybe add compression for all refseqs
-# TODO: remove too_few_species.txt
-echo "Done!"
+echo "Done!
