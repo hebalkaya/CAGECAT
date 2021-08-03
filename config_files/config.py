2b5cc27816f94d0ceb41d375d8eb0eaae97ae86d
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-08-03 16:41:06 +0200

    fix bug where overview of selected clusters (on downstream page of extract_clusters and clinker_query) would not be correct if only 1 cluster was selected

--- config_files/config.py
+++ config_files/config.py
@@ -4,7 +4,7 @@ Author: Matthias van den Belt
 """
 
 from config_files.sensitive import *
-CAGECAT_VERSION = '0.79'
+CAGECAT_VERSION = '0.80'
 
 # changeable
 CONF = {"SQLALCHEMY_DATABASE_URI": 'sqlite:////repo/cagecat/database.db',
