5db2748ec8581213b237967206758817618bed9a
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-08-03 16:01:52 +0200

    added todo to creating databases

--- hmm_database_creation/create_databases.py
+++ hmm_database_creation/create_databases.py
@@ -1,2 +1,5 @@
 # TODO: create script (use other create_database script as example)
 # but now we used files created by touch to see which databases should be created
+
+# TODO: compress all refseq gbks
+# tar cvf - $fp --remove-files | gzip -9 - > $fp.tar.gz
