729bea8ce6c6aefb761975fb5d797e51bee53a65
Belt, Matthias van den <matthias.vandenbelt@wur.nl>
2021-08-05 15:19:00 +0200

    fix typos, add diamond to create databases to dockerfile

--- hmm_database_creation/create_databases.py
+++ hmm_database_creation/create_databases.py
@@ -11,24 +11,10 @@ import time
 import os
 import sys
 import typing as t
+import shutil
 
 sys.path.append('..')
 from config_files.config import CONF, REFSEQ_DIR, CREATE_HMM_DB_SETTINGS
-
-def init():
-    """Creates directories if not present yet
-
-    Output:
-        - created directories
-    """
-    if not os.path.exists(CONF['finished_hmm_db_folder']):
-        os.makedirs(CONF['finished_hmm_db_folder'], exist_ok=True)
-
-    os.chdir(CONF['finished_hmm_db_folder'])
-
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-
 
 def list_files(_genus: str) -> t.List[str]:
     """Lists all present GenBank files for the given genus
@@ -49,30 +35,39 @@ def list_files(_genus: str) -> t.List[str]:
 
 
 if __name__ == '__main__':
-    init()
-    print('Removing databases')
+    print('Removing databases', flush=True)
+
+    os.chdir(CONF['finished_hmm_db_folder'])
     for f in os.listdir():
-        os.remove(f)
-        print(f'  Removed: {f}')# remove old db's
+        try:
+            os.remove(f)
+        except IsADirectoryError:
+            shutil.rmtree(f)
+
+        print(f'  Removed: {f}', flush=True)  # remove old db's
+
+    path = os.path.join(CONF['finished_hmm_db_folder'], 'logs')
+    if not os.path.exists(path):
+        os.makedirs(path, exist_ok=True)
 
     while True:
         dbs_to_create_path = os.path.join(REFSEQ_DIR, 'databases_to_create')
-        dbs_to_create = os.listdir()
+        dbs_to_create = os.listdir(dbs_to_create_path)
 
         if len(dbs_to_create) == 0:
-            print(f'Nothing to create. Sleeping for {CREATE_HMM_DB_SETTINGS["sleeping_time"]} seconds')
+            print(f'Nothing to create. Sleeping for {CREATE_HMM_DB_SETTINGS["sleeping_time"]} seconds', flush=True)
             time.sleep(CREATE_HMM_DB_SETTINGS['sleeping_time'])
         elif len(dbs_to_create) == 1 and dbs_to_create[0] == 'stop_creating_databases':
-            print('Encountered the stop_creating_databases file.')
+            print('Encountered the stop_creating_databases file', flush=True)
             subprocess.run(['rm', os.path.join(dbs_to_create_path, 'stop_creating_databases')])
-            print('Finished creating all databases. Exiting')
+            print('Finished creating all databases. Exiting', flush=True)
             exit(0)
         else:
             for genus in dbs_to_create:
                 if genus == 'stop_creating_databases':
                     continue  # as we don't want to make a db for this file
                     # actually create the db
-                print(f'Creating {genus} database')
+                print(f'Creating {genus} database', flush=True)
                 cmd = ["cblaster", "makedb",
                        "--name", genus,
                        "--cpus",  CREATE_HMM_DB_SETTINGS['cpus'],
@@ -81,19 +76,19 @@ if __name__ == '__main__':
                 cmd.extend(list_files(genus))
 
                 if len(cmd) == 8: # indicates no files were added
-                    print(f'{genus} has no genome files. Continuing..')
+                    print(f'{genus} has no genome files. Continuing..', flush=True)
                     continue
 
-                with open(os.path.join('logs', f'{genus}_creation.log'), 'w') as outf:
+                with open(os.path.join(CONF['finished_hmm_db_folder'], 'logs', f'{genus}_creation.log'), 'w') as outf:
                     res = subprocess.run(cmd, stderr=outf, stdout=outf, text=True)
 
                 if res.returncode != 0:
-                    print('Something went wrong. Exiting..')
+                    print('Something went wrong. Exiting..', flush=True)
                     exit(1)
 
-                print(f'Successfully created {genus} HMM database')
+                print(f'  Successfully created {genus} HMM database', flush=True)
                 subprocess.run(['rm', os.path.join(dbs_to_create_path, genus)])
-                print(f'Removed {genus} touch file')
+                print(f'  Removed {genus} touch file', flush=True)
 
 # TODO: create script (use other create_database script as example)
 # but now we used files created by touch to see which databases should be created
