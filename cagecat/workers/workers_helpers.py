4c5201b5cfb916e4bf319dbbf6968f0f61bbe21e
Matthias van den Belt <mvandenbelt@bio-prodict.nl>
2022-05-24 19:28:59 +0200

    support sanitization of multiple files

--- cagecat/workers/workers_helpers.py
+++ cagecat/workers/workers_helpers.py
@@ -4,6 +4,7 @@ Author: Matthias van den Belt
 """
 
 # package imports
+import shutil
 import subprocess
 import os
 
@@ -518,8 +519,20 @@ def sanitize_file(file_path, job_id, remove_old_files=False):
 
     sanitized_fn = os.path.join(sanitized_folder, job_id, f'{job_id}.gbk')
     destination = os.path.join(base, 'uploads', f'{job_id}.gbk')
-    os.rename(sanitized_fn, destination)
 
+    # for case when multiple files are uploaded (clinker)
+    start = 1
+    if os.path.exists(destination):
+        destination = os.path.join(base, 'uploads', f'{job_id}_{start}.gbk')
+        while os.path.exists(destination):
+            start += 1
+            destination = os.path.join(base, 'uploads', f'{job_id}_{start}.gbk')
+
+    os.rename(sanitized_fn, destination)
     print('Moved sanitized file from', sanitized_fn, 'to', destination)
+
+    base_folder = os.path.join(sanitized_folder, job_id)
+    shutil.rmtree(base_folder)
+    print('Removed folder', base_folder)
 
     return destination
