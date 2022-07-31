6e48463f825da7360fbe9d2ba27919347033a604
Matthias van den Belt <mvandenbelt@bio-prodict.nl>
2022-07-31 11:50:25 +0200

    bug fix where maintenance script would fail

--- maintenance/maint_backup.sh
+++ maintenance/maint_backup.sh
@@ -2,6 +2,7 @@
 # Author: Matthias van den Belt
 
 fp="/backups/$(date +"%Y%m%d")_backup"
+todays_backup="$(date +"%Y%m%d")_backup"
 
 echo "Creating directory: $fp"
 mkdir $fp
@@ -16,10 +17,10 @@ echo "Copying config.py to $fp/config.py (CAGECAT's version number)"
 cp /repo/config_files/config.py "$fp"
 
 echo "Writing cblaster version to $fp/cblaster_version.txt"
-cblaster --version > "$fp/cblaster_version.txt"
+/miniconda3/bin/cblaster --version > "$fp/cblaster_version.txt"
 
 echo "Listing HMM databases to $fp/hmm_databases.txt"
-ls -l /hmm_databases/ > "$fp/hmm_databases.txt"
+ls -lR /hmm_databases/ > "$fp/hmm_databases.txt"
 
 echo "Copying logs folder to $fp/process_logs"
 cp -r /process_logs "$fp"
@@ -30,9 +31,10 @@ apt list --installed > "$pckgs_linux"
 
 libs_python="$fp/python_libs.txt"
 echo "Writing installed Python libraries to $libs_python"
-pip freeze > "$libs_python"
+/miniconda3/bin/pip freeze > "$libs_python"
 
 echo "Compressing in to $fp.tar.gz"
-tar cvf - $fp --remove-files | gzip -9 - > $fp.tar.gz
+cd "/backups" || exit
+tar cvf - $todays_backup --remove-files | gzip -9 - > $fp.tar.gz
 
 echo "Finished. Backup available at $fp.tar.gz"
