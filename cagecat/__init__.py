3a986a8bfd343647b6eae41d863711c36fa732b7
Matthias van den Belt <mvandenbelt@bio-prodict.nl>
2022-09-16 13:55:30 +0200

    add dynamic sha256 hash of javascript code to security headers

--- cagecat/__init__.py
+++ cagecat/__init__.py
@@ -8,11 +8,15 @@ Author: Matthias van den Belt
 """
 
 # import statements
-from flask import Flask
+import base64
+import hashlib
+
+from flask import Flask, Response
 from flask_sqlalchemy import SQLAlchemy
 
 import redis
 import rq
+import re
 
 
 from config_files.config import init_config
@@ -46,3 +50,60 @@ if Statistic.query.filter_by(name="finished").first() is None:
         db.session.add(s)
 
     db.session.commit()
+
+# add_header Content-Security-Policy "script-src 'self' *.googleapis.com cdnjs.cloudflare.com cdn.jsdelivr.net; frame-src 'self'";
+# csp_headers = {
+#     'frame-src': [
+#         'self'
+#     ],
+#     'script-src': [
+#         "'sha256-VTAmOhFJf7NXPaOoDtmGnzgeTy5irawqE7Gps5UfNaU='",
+#         "'sha256-5NUfqwE5Ru7GwQSUKLQJ+U61xroMjwjJdR/FrbGlXgc='",
+#         0,
+#         # body.onload will be added here. index 2 will be set over and over for every response
+#         'self',
+#         '*.googleapis.com',
+#         'cdnjs.cloudflare.com',
+#         'cdn.jsdelivr.net',
+#     ]
+# }
+
+csp_headers =   "frame-src 'self'; " \
+                "script-src " \
+                "'unsafe-hashes' " \
+                "'sha256-VTAmOhFJf7NXPaOoDtmGnzgeTy5irawqE7Gps5UfNaU=' " \
+                "'sha256-5NUfqwE5Ru7GwQSUKLQJ+U61xroMjwjJdR/FrbGlXgc=' " \
+                "'sha256-{0}' " \
+                "'self' " \
+                "*.googleapis.com " \
+                "cdnjs.cloufdare.com " \
+                "cdn.jsdelivr.net"
+
+# js_pattern = r'<body .+onload="(.+)">'
+js_pattern = re.compile(r'<script type="application\/javascript">(function wrapped\(\).+)<\/script>')
+
+def hash_digest_js_code(resp):
+    """Returns sha256 digest of response JS code to set CSP header
+
+    :param resp:
+    :return:
+    """
+    resp: Response
+
+    matches = re.findall(pattern=js_pattern, string=resp.get_data(as_text=True))
+    if len(matches) == 0:
+        to_encode = ''
+    elif len(matches) == 1:
+        to_encode = matches[0]
+    else:
+        raise Exception('Invalid match length')
+
+    print('ENCODING:', to_encode)
+    return base64.b64encode(hashlib.sha256(to_encode.encode()).digest()).decode('UTF-8')
+
+@app.after_request
+def add_security_headers(resp):
+    headers = csp_headers.format(hash_digest_js_code(resp))
+    resp.headers['Content-Security-Policy'] = headers
+
+    return resp
