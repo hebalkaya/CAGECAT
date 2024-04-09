39763b292ded186ef83c05c8cc9253a488a8e38a
hebalkaya <hebalkaya@gmail.com>
2024-04-09 04:03:52 +0000

    Added debug configurations to run.py for development.

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
-"""Runs the CAGECAT web service
-
-Author: Matthias van den Belt
+"""
+Runs the CAGECAT web service
 """
 
 # own project imports
 from cagecat import app
+
+### Configure Flask App Debugging
+# Only for development mode. To be removed in deployment.
+
+app.config['DEBUG'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 ### main code
 if __name__ == "__main__":
